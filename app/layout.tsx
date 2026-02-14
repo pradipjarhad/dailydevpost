f67ae6291d509fb3d17b431eba130920452f03f3
Pradip <theprogrammerbro@gmail.com>
2026-02-14 18:12:03 +0530

    feat: Add root layout (`app/layout.tsx`) and migrate analytics from Vercel to Pliny.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
+
 
 const inter = Inter({
   subsets: ['latin'],
@@ -89,7 +89,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
                 <Header />
                 <main className="mb-auto">{children}</main>
-                <VercelAnalytics />
+
               </SearchProvider>
               <Footer />
             </div>
