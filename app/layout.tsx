7628327e222fd41bbbc33d7cb88f087d8e8fd6d1
Pradip <theprogrammerbro@gmail.com>
2026-06-16 03:32:59 +0530

    feat: implement root layout with SEO metadata, analytics, and layout wrapper component

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ThemeProviders>
           <Analytics gaId={siteMetadata.analytics?.googleAnalytics?.googleAnalyticsId || ''} />
           <SectionContainer>
-            <div className="flex h-screen flex-col justify-between font-sans">
+            <div className="flex min-h-screen flex-col justify-between font-sans">
               <SearchProviderWrapper searchConfig={siteMetadata.search as SearchConfig}>
                 <Header />
                 <main className="mb-auto">
